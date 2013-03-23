8d26ec46b192d2891cc8c88009ec72f01abf0e6e
Michael Barrett <loki77@gmail.com>
2013-03-23 11:06:46 -0700

    Dealing w/ resources module change

--- examples/test.py
+++ examples/test.py
@@ -1,9 +1,12 @@
-import nanomon
+from nanomon import resources
 from nanomon import registry
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
-webserver_group = nanomon.MonitoringGroup('webservers', port=80)
-www1 = nanomon.Host('www1', monitoring_groups=[webserver_group,], port=443, type='m1.xlarge')
-http_check = nanomon.Command('check_http',
-        'check_http {host:address} {port}', port=80)
-http_monitor = nanomon.Monitor('http_monitor', command=http_check, monitoring_groups=[webserver_group,])
+
+webserver_group = resources.MonitoringGroup('webservers', port=80)
+www1 = resources.Host('www1', monitoring_groups=[webserver_group,], port=443, type='m1.xlarge')
+http_check = resources.Command('check_http',
+        'check_http {host[address]} {port}', port=80)
+http_monitor = resources.Monitor('http_monitor', command=http_check, monitoring_groups=[webserver_group,])
