19c087941e193b79b6f76e75cc024878ef8c7c6f
Michael Barrett <loki77@gmail.com>
2013-04-02 09:24:29 -0700

    Update to use Node instead of Host

--- examples/test.py
+++ examples/test.py
@@ -1,12 +1,11 @@
 from nanomon import resources
-from nanomon import registry
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
 webserver_group = resources.MonitoringGroup('webservers', port=80)
-www1 = resources.Host('www1', monitoring_groups=[webserver_group,], port=443, type='m1.xlarge')
+www1 = resources.Node('www1', monitoring_groups=[webserver_group,], port=443, type='m1.xlarge')
 http_check = resources.Command('check_http',
         'check_http {host[address]} {port}', port=80)
 http_monitor = resources.Monitor('http_monitor', command=http_check, monitoring_groups=[webserver_group,])
