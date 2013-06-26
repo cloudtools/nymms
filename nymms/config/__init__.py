e8a1b41bf71bbace050f028a038f62da322f3d76
Michael Barrett <loki77@gmail.com>
2013-06-26 11:06:57 -0700

    New config system

---
+++ nymms/config/__init__.py
@@ -0,0 +1,27 @@
+import imp
+import os
+
+from nymms.exceptions import NymmsException
+
+class FileNotFound(NymmsException):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __str__(self):
+        return "Config file '%s' not found." % (self.filename)
+
+
+config_path = os.path.expanduser(os.environ.get('NYMMS_CONFIG',
+        '/etc/nymms/nymms'))
+
+path, module = os.path.split(config_path)
+
+if module.endswith('.py'):
+    module = module[:-3]
+
+try:
+    module_info = imp.find_module(module, [path])
+except ImportError:
+    raise FileNotFound(config_path)
+
+settings = imp.load_module('settings', *module_info)
