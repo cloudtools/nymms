505501b9fcbda130338dc2a3f44f2507c88b073b
Michael Barrett <loki77@gmail.com>
2013-06-26 15:06:59 -0700

    Allow for defaults in config.

--- nymms/config/__init__.py
+++ nymms/config/__init__.py
@@ -6,6 +6,8 @@ logger = logging.getLogger(__name__)
 
 from nymms.exceptions import NymmsException
 
+DEFAULTS = {}
+
 class FileNotFound(NymmsException):
     def __init__(self, filename):
         self.filename = filename
@@ -14,18 +16,61 @@ class FileNotFound(NymmsException):
         return "Config file '%s' not found." % (self.filename)
 
 
+class SettingNotFound(NymmsException):
+    def __init__(self, setting_name):
+        self.setting_name = setting_name
+
+    def __str__(self):
+        return "Setting '%s' not found." % (self.setting_name)
+
+
 config_path = os.path.expanduser(os.environ.get('NYMMS_CONFIG',
         '/etc/nymms/nymms'))
 
-path, module = os.path.split(config_path)
 
-if module.endswith('.py'):
-    module = module[:-3]
+class DefaultSettingsProxy(object):
+    def __init__(self, config_path=config_path, defaults=None):
+        self.config_path = config_path
+        self.settings = None
+        if not defaults:
+            self.defaults = {}
+        else:
+            self.defaults = defaults
 
-try:
-    module_info = imp.find_module(module, [path])
-except ImportError:
-    raise FileNotFound(config_path)
+        self._load_config()
 
-logger.debug("Loading config from path: %s" % (config_path))
-settings = imp.load_module('settings', *module_info)
+    def _load_config(self, force=False):
+        if self.settings and not force:
+            logger.debug('Settings already loaded and force=False. Skipping '
+                    'reload.')
+            return
+        path, module = os.path.split(config_path)
+        if module.endswith('.py'):
+            module = module[:-3]
+
+        try:
+            module_info = imp.find_module(module, [path])
+        except ImportError:
+            raise FileNotFound(config_path)
+
+        logger.debug("Loading config from path: %s" % (config_path))
+        self.settings = imp.load_module('settings', *module_info)
+
+    def __getattr__(self, attr):
+        """ First check the settings module, then the defaults dictionary.
+
+        If neither of those have the setting, raise SettingNotFound.
+        """
+        try:
+            try:
+                value = getattr(self.settings, attr)
+                logger.debug('Setting found in settings file.')
+            except AttributeError:
+                value = self.defaults[attr]
+                logger.debug("Setting '%s' found in defaults." % (attr))
+        except KeyError:
+            logger.error("Setting '%s' not found." % (attr))
+            raise SettingNotFound(attr)
+        return value
+
+settings = DefaultSettingsProxy(config_path, defaults=DEFAULTS)
