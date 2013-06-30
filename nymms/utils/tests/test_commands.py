36b49b485628392031573e5cacc761302ef967e8
Michael Barrett <loki77@gmail.com>
2013-06-30 13:12:49 -0700

    Simple tests for commands.py

---
+++ nymms/utils/tests/test_commands.py
@@ -0,0 +1,18 @@
+import unittest
+
+from nymms.utils import commands
+
+class TestCommands(unittest.TestCase):
+    def test_execute_failure(self):
+        with self.assertRaises(commands.CommandFailure):
+            # Non-existant command
+            out, err = commands.execute('xxxps auwwwx', 10)
+
+    def test_execute_timeout(self):
+        with self.assertRaises(commands.CommandTimeout):
+            out, err = commands.execute('sleep 2', 1)
+
+    def test_execute_success(self):
+        out, err = commands.execute('echo test', 10)
+        self.assertEqual(out, 'test\n')
+        self.assertEqual(err, '')
