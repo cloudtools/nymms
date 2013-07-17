ce70abdceb8be6d3c9d0aaf4e55d20ceb873be02
Michael Barrett <loki77@gmail.com>
2013-07-17 16:35:39 -0700

    More sweeping checkins - I suck

---
+++ nymms/config/tests/test_yaml_config.py
@@ -0,0 +1,19 @@
+import unittest
+import os
+
+from nymms.config import yaml_config
+
+
+class TestIncludeLoader(unittest.TestCase):
+    def setUp(self):
+        self.root = os.path.split(__file__)[0]
+
+    def test_relative_include(self):
+        full_path = os.path.join(self.root, 'config.yaml')
+        version, relative_config = yaml_config.load_config(full_path)
+        self.assertEqual(relative_config['foo'], 'bar')
+        self.assertEqual(relative_config['file1'], 1)
+
+    def test_missing_config(self):
+        with self.assertRaises(IOError):
+            missing_config = yaml_config.load_config('nonexistant.yaml')
