781d3e3d465fe15075cfa50bd82bef28aa2ff23a
Michael Barrett <loki77@gmail.com>
2013-09-04 15:51:13 -0700

    We haven't updated our version for a bit.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nymms',
-    version='0.1.0',
+    version='0.4.0',
     author='Michael Barrett',
     author_email='loki77@gmail.com',
     license="New BSD license",
