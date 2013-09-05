682c02b01775a443ce17aa5ea9805e5be6fd120b
Michael Barrett <loki77@gmail.com>
2013-09-04 20:47:24 -0700

    Fix versioning and add scripts directory

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 import os
 from setuptools import setup, find_packages
+import glob
+
+src_dir = os.path.dirname(__file__)
 
 def read(filename):
-    full_path = os.path.join(os.path.dirname(__file__), filename)
+    full_path = os.path.join(src_dir, filename)
     with open(full_path) as fd:
         return fd.read()
 
 
 setup(
     name='nymms',
-    version='0.4.2',
+    version='0.2.1',
     author='Michael Barrett',
     author_email='loki77@gmail.com',
     license="New BSD license",
@@ -21,4 +24,5 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Development Status :: 3 - Alpha"],
     packages=find_packages(),
+    scripts=glob.glob(os.path.join(src_dir, 'scripts', '*')),
 )
