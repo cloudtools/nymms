82ab204f8021a5a400a11b3f0d96ff0493bfab9c
Michael Barrett <loki77@gmail.com>
2013-09-17 00:49:57 -0700

    Fix nosetest failures

--- nymms/utils/tests/test_commands.py
+++ nymms/utils/tests/test_commands.py
@@ -7,13 +7,12 @@ class TestCommands(unittest.TestCase):
     def test_execute_failure(self):
         with self.assertRaises(commands.CommandFailure):
             # Non-existant command
-            out, err = commands.execute('xxxps auwwwx', 10)
+            out = commands.execute('xxxps auwwwx', 10)
 
     def test_execute_timeout(self):
         with self.assertRaises(commands.CommandTimeout):
-            out, err = commands.execute('sleep 2', 1)
+            out = commands.execute('sleep 2', 1)
 
     def test_execute_success(self):
-        out, err = commands.execute('echo test', 10)
+        out = commands.execute('echo test', 10)
         self.assertEqual(out, 'test\n')
-        self.assertEqual(err, '')
