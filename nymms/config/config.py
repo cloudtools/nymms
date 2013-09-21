a1e700763347005cf6cfa19f484359590f8c612d
Michael Barrett <loki77@gmail.com>
2013-09-21 10:48:07 -0700

    Add defaults, deal w/ only default config

--- nymms/config/config.py
+++ nymms/config/config.py
@@ -5,7 +5,26 @@ logger = logging.getLogger(__name__)
 
 from nymms.config import yaml_config
 
-DEFAULTS = {}
+DEFAULTS = {
+    'monitor_timeout': 15,
+    'queue_region': 'us-east-1',
+    'resources': '/etc/nymms/resources.yaml',
+    'nodes': '/etc/nymms/nodes.yaml',
+    'state_domain': 'nymms_state',
+    'tasks_queue': 'nymms_tasks',
+    'results_topic': 'nymms_results',
+
+    'probe.max_retries': 3,
+    'probe.queue_wait_time': 20,
+    'probe.retry_delay': 30,
+
+    'reactor.handler_config_path': '/etc/nymms/handlers',
+    'reactor.queue_name': 'reactor_queue',
+    'reactor.queue_wait_time': 20,
+    'reactor.visibility_timeout': 30,
+
+    'scheduler.interval': 300,
+}
 
 settings = None
 version = None
@@ -16,7 +35,8 @@ def load_config(path, force=False):
     if settings and not force:
         return
     settings = copy.deepcopy(DEFAULTS)
-    version, __config_settings = yaml_config.load_config(path)
-    settings.update(__config_settings)
+    version, _config_settings = yaml_config.load_config(path)
+    if _config_settings:
+        settings.update(_config_settings)
     logger.debug("Config loaded from '%s' with version '%s'." % (path,
                                                                  version))
