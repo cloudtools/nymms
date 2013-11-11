e0b995f6c3434294ad5dd9bb26abae73016fc307
Michael Barrett <loki77@gmail.com>
2013-11-10 17:22:58 -0800

    New exception for missing context

--- nymms/exceptions.py
+++ nymms/exceptions.py
@@ -12,3 +12,11 @@ class OutOfDateState(NymmsException):
     def __str__(self):
         return "Previous (%d) state newer than current (%d)." % (
             self.previous_state.last_update, self.current_state.last_update)
+
+
+class MissingCommandContext(NymmsException):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return "Invalid command variable: %s" % self.message
