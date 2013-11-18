59d844ccef9b300446dc0aaa372c29e3c40f3868
Michael Barrett <loki77@gmail.com>
2013-11-17 18:19:54 -0800

    Fixes #17 - deep update of default config

--- nymms/config/config.py
+++ nymms/config/config.py
@@ -1,10 +1,22 @@
 import logging
 import copy
 import os
+import collections
 
 logger = logging.getLogger(__name__)
 
 from nymms.config import yaml_config
+
+
+def deep_update(orig, upd):
+    """ Does a 'deep' update of dictionary 'orig' with dictionary 'upd'."""
+    for k, v in upd.iteritems():
+        if isinstance(v, collections.Mapping):
+            r = deep_update(orig.get(k, {}), v)
+            orig[k] = r
+        else:
+            orig[k] = upd[k]
+    return orig 
 
 default_conf_dir = '/etc/nymms'
 
@@ -22,6 +34,7 @@ DEFAULTS = {
         'max_retries': 2,
         'queue_wait_time': 20,
         'retry_delay': 30,
+        'blah': 22,
     },
 
     'reactor': {
@@ -51,6 +64,6 @@ def load_config(path, force=False):
     settings = copy.deepcopy(DEFAULTS)
     version, _config_settings = yaml_config.load_config(path)
     if _config_settings:
-        settings.update(_config_settings)
+        deep_update(settings, _config_settings)
     logger.debug("Config loaded from '%s' with version '%s'." % (path,
                                                                  version))
