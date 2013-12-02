d5acc2e49657d19490a06e86e6811df42f7a0586
Michael Barrett <loki77@gmail.com>
2013-12-02 11:14:52 -0800

    Provide tests for indented includes

--- nymms/config/tests/test_yaml_config.py
+++ nymms/config/tests/test_yaml_config.py
@@ -17,3 +17,8 @@ class TestIncludeLoader(unittest.TestCase):
     def test_missing_config(self):
         with self.assertRaises(IOError):
             yaml_config.load_config('nonexistant.yaml')
+
+    def test_indent_include(self):
+        full_path = os.path.join(self.root, 'config.yaml')
+        version, relative_config = yaml_config.load_config(full_path)
+        self.assertEqual(relative_config['included']['a'], 1)
