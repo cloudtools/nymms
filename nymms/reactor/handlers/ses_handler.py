a197ea1af4564f13e7c1e53090ffbabd2f1f8fc6
Michael Barrett <loki77@gmail.com>
2014-10-12 12:50:51 -0700

    Preparing for per-monitor email recipients

--- nymms/reactor/handlers/ses_handler.py
+++ nymms/reactor/handlers/ses_handler.py
@@ -24,28 +24,31 @@ class SESHandler(Handler):
       recipients: list, email addresses
       filters: list, filters
     """
-    def _connect(self):
-        if getattr(self, '_aws_conn', None):
-            return
-        self._aws_conn = ConnectionManager(region=self.config['region'])
+    @property
+    def aws_conn(self):
+        if not getattr(self, '_aws_conn', None):
+            self._aws_conn = ConnectionManager(region=self.config['region'])
+        return self._aws_conn
 
     def _send_email(self, result, previous_state):
-        self._connect()
         subject = Template(self.config['subject_template'])
         subject.environment.undefined = SimpleUndefined
         body = Template(self.config['body_template'])
         body.environment.undefined = SimpleUndefined
         sender = self.config['sender']
-        recipients = self.config['recipients']
+        recipients = self.config.get('recipients', [])
         result_data = result.serialize()
-        logger.debug("Sending SES alert to %s as %s for %s.",
-                     recipients, sender, result.id)
-        self._aws_conn.ses.send_email(
-            source=sender,
-            subject=subject.render(result_data),
-            body=body.render(result_data),
-            to_addresses=recipients
-        )
+        if recipients:
+            logger.debug("Sending SES alert to %s as %s for %s.",
+                         recipients, sender, result.id)
+            self.aws_conn.ses.send_email(
+                source=sender,
+                subject=subject.render(result_data),
+                body=body.render(result_data),
+                to_addresses=recipients)
+        else:
+            logger.debug("No valid recipients found, not sending email for "
+                         "%s.", result.id)
 
     def process(self, result, previous_state):
         self._send_email(result, previous_state)
