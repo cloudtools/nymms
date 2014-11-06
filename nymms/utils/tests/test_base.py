04219e9d759c69b17cb94d34cc9b9d8202cb208c
Michael Barrett <loki77@gmail.com>
2014-11-06 14:20:08 -0800

    Add parse_time tests

--- nymms/utils/tests/test_base.py
+++ nymms/utils/tests/test_base.py
@@ -1,6 +1,7 @@
 import unittest
 
 from nymms import utils
+from nymms.exceptions import InvalidTimeFormat
 
 
 class TestBase(unittest.TestCase):
@@ -8,3 +9,15 @@ class TestBase(unittest.TestCase):
         from logging import Logger
         cls = utils.load_object_from_string('logging.Logger')
         self.assertIs(cls, Logger)
+
+    def test_parse_time(self):
+        base_time = 1415311935
+        self.assertEqual(utils.parse_time('+60s', base_time), base_time + 60)
+        self.assertEqual(utils.parse_time('+10m', base_time),
+                         base_time + (60 * 10))
+        self.assertEqual(utils.parse_time('+10h', base_time),
+                         base_time + (60 * 60 * 10))
+        self.assertEqual(utils.parse_time('-10d', base_time),
+                         base_time - (10 * 60 * 60 * 24))
+        with self.assertRaises(InvalidTimeFormat):
+            utils.parse_time('+2000xxx')
