f04b73087674f58aeab169239a1c6843b87d6d3d
Michael Barrett <loki77@gmail.com>
2014-11-09 14:39:58 -0800

    Forgot State.py in last update

---
+++ nymms/state/State.py
@@ -0,0 +1,19 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+from nymms.schemas import StateRecord
+
+
+class StateBackend(object):
+    def __init__(self):
+        logger.debug("%s initialized.", self.__class__.__name__)
+
+    def deserialize_state(self, item):
+        try:
+            state = StateRecord(item, strict=False)
+            state.validate()
+            return state
+        except Exception:
+            logger.exception("Problem deserializing state:")
+            logger.error("State data: %s", str(item))
